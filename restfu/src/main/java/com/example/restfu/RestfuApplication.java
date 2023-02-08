043129a37c22bcc68ea7d7a3b97a899ebdcfd949
Victor H. T. Filho <hugovtotal@gmail.com>
2023-02-08 09:52:58 -0300

    .

---
+++ restfu/src/main/java/com/example/restfu/RestfuApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfuApplication.class, args);
+	}
+
+}
---
+++ restfu/src/test/java/com/example/restfu/RestfuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

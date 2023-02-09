94552944956ecfdbb3e7dc2e1bcf00786833f5c2
Victor H. T. Filho <hugovtotal@gmail.com>
2023-02-09 20:30:37 -0300

    jpa+spweb+h2+rest

---
+++ Piratex/src/main/java/com/example/Piratex/PiratexApplication.java
@@ -0,0 +1,13 @@
+package com.example.Piratex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiratexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiratexApplication.class, args);
+	}
+
+}
---
+++ Piratex/src/test/java/com/example/Piratex/PiratexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Piratex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiratexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
